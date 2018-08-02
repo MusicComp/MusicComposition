d3f1e95c23b474dbcc92e0232935045e2d74a88a
Kyle Feng <kylefeng28@gmail.com>
2018-08-02 13:18:39 -0400

    Fix chords conversions

--- rhythm/convert.py
+++ rhythm/convert.py
@@ -7,24 +7,28 @@ def convert_from_score(score):
     for part in score.parts:
         s += f'\\instrument<"piano">\n'
         s += '[ '
-        for el in part.notes:
+        for el in part.flat:
             if el.duration.quarterLength != 0.0:
                 dur = 4 / Fraction(el.duration.quarterLength)
 
                 # Single note
                 if isinstance(el, m21.note.Note):
                     n = el.nameWithOctave
+                # Rest
+                elif isinstance(el, m21.note.Rest):
+                    n = 'r'
                 # Chord
                 elif isinstance(el, m21.chord.Chord):
                     n = '{'
-                    for pitch in el.pitches:
-                        n += pitch.nameWithOctave + ','
+                    n += ','.join([ pitch.nameWithOctave for pitch in el.pitches ])
                     n += '}'
+                else:
+                    pass
                 
                 # Add duration
                 s += f"{n}*{dur.denominator}/{dur.numerator} "
 
-        s += ' ]'
+        s += ']\n'
     return s
 
 if __name__ == '__main__':
