9028c87cf4a7d1a8f07ed4bd5d60a0f83ceed894
Kyle Feng <kylefeng28@gmail.com>
2018-08-02 11:06:41 -0400

    Add conversion script

--- rhythm/convert.py
+++ rhythm/convert.py
@@ -1,25 +1,33 @@
 from fractions import Fraction
 import music21 as m21
+import sys
 
 def convert_from_score(score):
     s = ''
     for part in score.parts:
         s += f'\\instrument<"piano">\n'
         s += '[ '
-        for note in part.notes:
-            if isinstance(note, m21.note.Note):
-                if note.duration.quarterLength != 0.0:
-                    dur = 4 / Fraction(note.duration.quarterLength)
-                    n = note.nameWithOctave + '*' + str(dur.denominator) + '/' + str(dur.numerator)
-                    s += n + ' '
-                else:
-                    # WTF?
-                    pass
-            # TODO handle chords
+        for el in part.notes:
+            if el.duration.quarterLength != 0.0:
+                dur = 4 / Fraction(el.duration.quarterLength)
+
+                # Single note
+                if isinstance(el, m21.note.Note):
+                    n = el.nameWithOctave
+                # Chord
+                elif isinstance(el, m21.chord.Chord):
+                    n = '{'
+                    for pitch in el.pitches:
+                        n += pitch.nameWithOctave + ','
+                    n += '}'
+                
+                # Add duration
+                s += f"{n}*{dur.denominator}/{dur.numerator} "
+
         s += ' ]'
     return s
 
 if __name__ == '__main__':
-    score = m21.midi.translate.midiFilePathToStream('broken_moon.mid')
+    score = m21.midi.translate.midiFilePathToStream(sys.argv[1])
     s = convert_from_score(score)
     print(s)
